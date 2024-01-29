0bc5265e9063807eb34e3109a8e94d7cb942100b
RITIKKUMAWAT009 <ritikkumawat140@gmail.com>
2024-01-30 00:37:58 +0530

    default code

---
+++ src/main/java/com/example/careersyncapp/CareerSyncAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.careersyncapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerSyncAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerSyncAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/careersyncapp/CareerSyncAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.careersyncapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerSyncAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
