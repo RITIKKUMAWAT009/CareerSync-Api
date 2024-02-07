8e2e50ef173c6607e746fd0383587e99f5693ed7
RITIKKUMAWAT009 <ritikkumawat140@gmail.com>
2024-02-07 09:22:53 +0530

    login and register api added

--- src/main/java/com/example/careersyncapp/Constants.java
+++ src/main/java/com/example/careersyncapp/Constants.java
@@ -1,8 +1,4 @@
 package com.example.careersyncapp;
-
-import com.example.careersyncapp.model.UserT;
-import jakarta.websocket.OnClose;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,6 +6,10 @@ import java.util.Map;
 public class Constants {
     public static final String RECORDS_FOUND = "Records Found";
     public static final String DATA = "Data";
+    public static final String SUCCESS = "Login successfully";
+    public static final String FAILURE = "Invalid username or password";
+    public static final String ALREADY_TAKEN = "Username is already taken";
+    public static final String CREATED_SUCCESS = "User created successfully";
 
     public static Map<String,Object>createMap(List<Object> data){
         Map<String,Object>response=new HashMap<>();
